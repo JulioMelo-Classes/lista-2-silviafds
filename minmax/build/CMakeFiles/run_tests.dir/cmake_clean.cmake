4b4f2c7b3ba83817795e8c6f2c6b08ddaf1f0736
silviafds <silviafernandes10@outlook.com.br>
2021-06-26 11:48:33 -0300

    questão minmax

---
+++ minmax/build/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/home/silvia/Imagens/lista-2-silviafds/test_manager/src/test_manager.cpp.o"
+  "CMakeFiles/run_tests.dir/src/main.cpp.o"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
